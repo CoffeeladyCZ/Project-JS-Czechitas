02a32803768936eee5a7baaf6ff3c3e141dbf4a5
Marcela <62770219+CoffeeladyCZ@users.noreply.github.com>
2021-05-23 22:27:53 +0200

    Add files via upload

---
+++ renderer-process/components/day/day.js
@@ -0,0 +1,38 @@
+export class Day extends HTMLElement {
+    constructor(date) {
+        super();
+        this.innerHTML = `
+        <div id="day-number"></div>
+        `;
+        this.date = date;
+        this.number = date.getDate();
+        this.date.getDay();
+        this.querySelector('#day-number').innerText = this.number;
+        this.addEventListener('click', this.handleClickEvent);
+    }
+
+    getDayName() {
+        switch(this.date.getDate()) {
+            case 0:
+                return 'Neděle';
+            case 1:
+                return 'Pondělí';
+            case 2:
+                return 'Úterý';
+            case 3:
+                return 'Středa';
+            case 4:
+                return 'Čtvrtek';
+            case 5:
+                return 'Pátek';
+            case 6:
+                return 'Sobota';
+        }
+    }
+
+    handleClickEvent() {
+        alert('Dnes je: ' + this.number); 
+    }
+}
+
+customElements.define('app-day', Day);
